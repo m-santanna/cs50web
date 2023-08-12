55364cd2ec10e97f2e19071d8edc04a6b58503e3
Matheus <matheus.santanna03@gmail.com>
2023-08-12 16:47:16 +0100

    search feature added

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,19 +1,51 @@
 from django.shortcuts import render
-from django.http import HttpResponseNotFound
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from . import util
+from django import forms
+from django.urls import reverse
 
+class Search(forms.Form):
+    searchbar = forms.CharField(widget=forms.TextInput(attrs={'class' : 'search', 'placeholder': 'Search'}))
 
 def index(request):
-    if request.method == 'GET':
-        return render(request, "encyclopedia/index.html", {
-            "entries": util.list_entries()
-        })
+
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries(),
+        "searchbar":Search()
+    })
 
 def wiki(request, wiki):
-    if request.method == 'GET':
-        if util.get_entry(wiki):
-            return render(request, 'encyclopedia/wiki.html', {
-                "wiki":wiki.capitalize(),
-                "content":util.get_entry(wiki)
+   
+    if util.get_entry(wiki):
+        for entry in util.list_entries():
+            if wiki.lower() == entry.lower():
+                wiki = entry
+        return render(request, 'encyclopedia/wiki.html', {
+            "wiki":wiki,
+            "content":util.get_entry(wiki),
+            "searchbar":Search()
+        })
+    else: return HttpResponseNotFound()
+
+def search(request):
+    strings = []
+    if request.method == 'POST':
+        form = Search(request.POST)
+        if form.is_valid():
+            substring = form.cleaned_data["searchbar"].lower()
+ 
+            for entry in util.list_entries():
+                if substring == entry.lower():
+                    return HttpResponseRedirect(f'wiki/{entry}')
+                
+                elif substring in entry.lower():
+                    strings.append(entry)
+            return render(request, 'encyclopedia/search.html', {
+                "strings":strings,
+                "searchbar":Search()
             })
-        else: return HttpResponseNotFound()
+    else:
+        return render(request, 'encyclopedia/search.html', {
+            "strings":strings,
+            "searchbar":Search()
+        })
