848c64fe6682ebed62dabf3c2a6668b800451daf
Matheus <matheus.santanna03@gmail.com>
2023-08-12 12:03:22 +0100

    index and entry page updated

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "wiki"
 urlpatterns = [
     path("", views.index, name="index"),
-    path("<str:wiki>", views.wiki, name="wiki")
+    path("wiki/<str:wiki>", views.wiki, name="wiki")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -4,14 +4,16 @@ from . import util
 
 
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    if request.method == 'GET':
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
 
 def wiki(request, wiki):
-    if util.get_entry(wiki):
-        return render(request, 'encyclopedia/wiki.html', {
-            "wiki":wiki.capitalize(),
-            "content":util.get_entry(wiki)
-        })
-    else: return HttpResponseNotFound()
+    if request.method == 'GET':
+        if util.get_entry(wiki):
+            return render(request, 'encyclopedia/wiki.html', {
+                "wiki":wiki.capitalize(),
+                "content":util.get_entry(wiki)
+            })
+        else: return HttpResponseNotFound()
