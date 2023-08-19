dc3980fdcd1885a41363bc75733c6dd55193e88d
Matheus <matheus.santanna03@gmail.com>
2023-08-19 18:55:20 +0100

    models corrected (close to final version)

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -8,6 +8,8 @@ class User(AbstractUser):
 
 class Category(models.Model):
     title = models.CharField(max_length=64)
+    def __str__(self):
+        return f'{self.title}'
 
 
 class Listing(models.Model):
@@ -16,16 +18,20 @@ class Listing(models.Model):
     description = models.CharField(max_length=512)
     online = models.BooleanField(default=True)
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    category = models.ForeignKey(User, on_delete=models.CASCADE)
-
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    def __str__(self):
+        return f'{self.title} owned by {self.owner}'
 
 class Bid(models.Model):
     current_bid = models.IntegerField()
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_bids')
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='listing_bid')
-
+    def __str__(self):
+        return f'Current bid for {self.listing} is {self.current_bid}'
 
 class Comments(models.Model):
     text = models.CharField(max_length=512)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comments')
     listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='listing_comments')
+    def __str__(self):
+        return f'Commented by {self.user} on {self.listing}: {self.text}'
