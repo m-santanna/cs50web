bb6b46e49f796d5a7d75e540f1c7d43ad4296f45
Matheus <matheus.santanna03@gmail.com>
2023-08-19 16:52:39 +0100

    commerce first commit v2

--- commerce/auctions/admin.py
+++ commerce/auctions/admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import *
-# Register your models here.
 
-admin.site.register(User, UserAdmin)
-admin.site.register(Bid)
-admin.site.register(Listing)
-admin.site.register(Comments)
+# Register your models here.
