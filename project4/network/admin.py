8ec9e34f07518c5d57ebec515790988a9b86178a
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2023-09-02 04:28:15 +0100

    admin permissions added / also not migrated

--- project4/network/admin.py
+++ project4/network/admin.py
@@ -3,3 +3,6 @@ from .models import *
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Posts)
+admin.site.register(Likes)
+admin.site.register(Comments)
+admin.site.register(Follow)
