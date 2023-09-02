ae152493c46b27536a041428b1fc520e328bb6ae
Matheus Sant'Anna <matheus.santanna03@gmail.com>
2023-09-02 04:39:52 +0100

    first migration

--- project4/project4/settings.py
+++ project4/project4/settings.py
@@ -82,6 +82,7 @@ DATABASES = {
 }
 
 AUTH_USER_MODEL = "network.User"
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
